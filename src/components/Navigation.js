605cc4938596472bda7eb303797f07ddbe053494
Isabella Chen <miss.izzyc@gmail.com>
2021-02-08 01:51:23 +0100

    new build

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,37 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "gatsby"
 import Headroom from "react-headroom"
 
 export default ({ location }) => {
+  const [pinStart, setPinStart] = useState(50)
+
   const currentPath = (location && location.pathname) || ""
 
+  // useEffect(() => {
+  //   try {
+  //     if (window.innerWidth >= 1366) {
+  //       setPinStart(270)
+  //     }
+  //   } catch (e) {
+  //     console.error(e)
+  //   }
+  // })
+
   return (
-    <Headroom pinStart={280}>
-      <nav className="nav">
-        <ul className="uk-navbar-nav">
-          <li className={`${currentPath === "/" ? "uk-active" : ""}`}>
-            <Link to="/">home</Link>
-          </li>
-          <li className={`${currentPath === "/resume" ? "uk-active" : ""}`}>
-            <Link to="/resume">resume</Link>
-          </li>
-          <li className={`${currentPath === "/portfolio" ? "uk-active" : ""}`}>
-            <Link to="/portfolio">portfolio</Link>
-          </li>
-        </ul>
-      </nav>
-    </Headroom>
+    // <Headroom pinStart={pinStart}>
+    <nav className="nav">
+      <ul className="nav-inner uk-navbar-nav">
+        <li className={`${currentPath === "/" ? "uk-active" : ""}`}>
+          <Link to="/">home</Link>
+        </li>
+        <li className={`${currentPath === "/resume" ? "uk-active" : ""}`}>
+          <Link to="/resume">resume</Link>
+        </li>
+        <li className={`${currentPath === "/portfolio" ? "uk-active" : ""}`}>
+          <Link to="/portfolio">portfolio</Link>
+        </li>
+      </ul>
+    </nav>
+    // </Headroom>
   )
 }
